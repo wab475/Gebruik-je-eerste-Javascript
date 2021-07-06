d5c055cdb5176c3fa0a1d66cf0f64246df5d5baf
wab475 <84964630+wab475@users.noreply.github.com>
2021-07-06 22:58:18 +0200

    Opdracht_012 Gebruik je eerste Javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("Hello Winc Academy");
+
+let firstName = 'Wilma';
+console.log(firstName);
+
+let som = 4 + 4;
+console.log(som);
+
+let somString = "4" + "4";
+console.log(somString);
+
+let addition = 10 + 20;
+console.log(addition);
+
+let subtraction = 100 - 50;
+console.log(subtraction);
+
+let multiplication = 20 * 4;
+console.log(multiplication);
+
+let division = 50 / 2;
+console.log(division);
+
+let modulus = 5 % 3;
+console.log(modulus);
+
+let leeftijd = '46';
+console.log(typeof leeftijd);
